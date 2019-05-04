0ab3ad4d34da6683211cf15243c2b75db9de3683
heaven <djyesu@gmail.com>
2019-05-04 16:19:09 +0800

    init

--- gen.cert.sh
+++ gen.cert.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# 扩展信任IP或域名
+## 一般ssl证书只信任域名的访问请求，有时候需要使用ip去访问server，那么需要给ssl证书添加扩展IP，
+## 多个IP用逗号隔开。如果想多个域名访问，则添加扩展域名（SSL_DNS）,多个SSL_DNS用逗号隔开
+export SSL_IP='127.0.0.1,10.252.97.142' # 例如: 1.2.3.4
+export SSL_DNS='boe.com.cn' # 例如: demo.rancher.com
+
+
+export CN=${1} # 例如: demo.rancher.com
+
+export SSL_CONFIG=./openssl.cnf
+
+export SSL_SUBJECT=${CN}
+
 if [ -z "$1" ]
 then
     echo
@@ -11,6 +24,27 @@ then
     echo '                      Multiple domains are acceptable'
     exit;
 fi
+
+if [[ -n ${SSL_DNS} || -n ${SSL_IP} ]]; then
+    cat >> ${SSL_CONFIG} <<EOM
+subjectAltName = @alt_names
+[alt_names]
+EOM
+    IFS=","
+    dns=(${SSL_DNS})
+    dns+=(${SSL_SUBJECT})
+    for i in "${!dns[@]}"; do
+      echo DNS.$((i+1)) = ${dns[$i]} >> ${SSL_CONFIG}
+    done
+
+    if [[ -n ${SSL_IP} ]]; then
+        ip=(${SSL_IP})
+        for i in "${!ip[@]}"; do
+          echo IP.$((i+1)) = ${ip[$i]} >> ${SSL_CONFIG}
+        done
+    fi
+fi
+
 
 SAN=""
 for var in "$@"
@@ -24,7 +58,20 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Generate root certificate if not exists
 if [ ! -f "out/root.crt" ]; then
-    bash gen.root.sh
+
+    if [ ! -d "out" ]; then
+        bash flush.sh
+    fi
+
+    # Generate root cert along with root key
+    openssl req -config ${SSL_CONFIG} \
+        -newkey rsa:2048 -nodes -keyout out/root.key.pem \
+        -new -x509 -days 7300 -out out/root.crt \
+        -subj "/C=CN/ST=Guangdong/L=Guangzhou/O=Fishdrowned/CN=Fishdrowned ROOT CA"
+
+    # Generate cert key
+    openssl genrsa -out "out/cert.key.pem" 2048
+
 fi
 
 # Create domain directory
@@ -37,13 +84,13 @@ mkdir -p ${DIR}
 openssl req -new -out "${DIR}/$1.csr.pem" \
     -key out/cert.key.pem \
     -reqexts SAN \
-    -config <(cat openssl.cnf \
+    -config <(cat ${SSL_CONFIG} \
         <(printf "[SAN]\nsubjectAltName=${SAN}")) \
     -subj "/C=CN/ST=Guangdong/L=Guangzhou/O=Fishdrowned/OU=$1/CN=*.$1"
 
 # Issue certificate
-# openssl ca -batch -config ./openssl.cnf -notext -in "${DIR}/$1.csr.pem" -out "${DIR}/$1.cert.pem"
-openssl ca -config ./openssl.cnf -batch -notext \
+# openssl ca -batch -config .${SSL_CONFIG} -notext -in "${DIR}/$1.csr.pem" -out "${DIR}/$1.cert.pem"
+openssl ca -config ${SSL_CONFIG} -batch -notext \
     -in "${DIR}/$1.csr.pem" \
     -out "${DIR}/$1.crt" \
     -cert ./out/root.crt \
