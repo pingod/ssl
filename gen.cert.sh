f52b5f8bc63ca53eb8d19929041c672236ed08b9
heaven <djyesu@gmail.com>
2019-05-04 20:11:08 +0800

    init

--- gen.cert.sh
+++ gen.cert.sh
@@ -3,22 +3,16 @@
 # 扩展信任IP或域名
 ## 一般ssl证书只信任域名的访问请求，有时候需要使用ip去访问server，那么需要给ssl证书添加扩展IP，
 ## 多个IP用逗号隔开。如果想多个域名访问，则添加扩展域名（SSL_DNS）,多个SSL_DNS用逗号隔开
-export SSL_IP='127.0.0.1,10.252.97.142' # 例如: 1.2.3.4
-export SSL_DNS='boe.com.cn' # 例如: demo.rancher.com
-
-
-export CN=${1} # 例如: demo.rancher.com
+export SSL_IP='127.0.0.1,10.252.97.142' # 例如: 192.168.1.111
+export SSL_DNS='boe.com.cn,*.boe.com.cn' # 例如: demo.rancher.com
 
 export SSL_CONFIG=./openssl.cnf
 
-export SSL_SUBJECT=${CN}
-
-if [ -z "$1" ]
-then
+if [ -z "$1" ];then
     echo
     echo 'Issue a wildcard SSL certificate with Fishdrowned ROOT CA'
     echo
-    echo 'Usage: ./gen.cert.sh <domain> [<domain2>] [<domain3>] [<domain4>] ...'
+    echo 'Usage: ./gen.cert.sh   <domain>'
     echo '    <domain>          The domain name of your site, like "example.dev",'
     echo '                      you will get a certificate for *.example.dev'
     echo '                      Multiple domains are acceptable'
@@ -26,32 +20,25 @@ then
 fi
 
 if [[ -n ${SSL_DNS} || -n ${SSL_IP} ]]; then
-    cat >> ${SSL_CONFIG} <<EOM
+    if [[ ! $(grep 'alt_names' ${SSL_CONFIG}) ]];then
+        cat >> ${SSL_CONFIG} <<EOM
 subjectAltName = @alt_names
-[alt_names]
+[ alt_names ]
 EOM
-    IFS=","
-    dns=(${SSL_DNS})
-    dns+=(${SSL_SUBJECT})
-    for i in "${!dns[@]}"; do
-      echo DNS.$((i+1)) = ${dns[$i]} >> ${SSL_CONFIG}
-    done
-
-    if [[ -n ${SSL_IP} ]]; then
-        ip=(${SSL_IP})
-        for i in "${!ip[@]}"; do
-          echo IP.$((i+1)) = ${ip[$i]} >> ${SSL_CONFIG}
+        IFS=","
+        dns=(${SSL_DNS})
+        for i in "${!dns[@]}"; do
+        echo DNS.$((i+1)) = ${dns[$i]} >> ${SSL_CONFIG}
         done
+
+        if [[ -n ${SSL_IP} ]]; then
+            ip=(${SSL_IP})
+            for i in "${!ip[@]}"; do
+            echo IP.$((i+1)) = ${ip[$i]} >> ${SSL_CONFIG}
+            done
+        fi
     fi
 fi
-
-
-SAN=""
-for var in "$@"
-do
-    SAN+="DNS:*.${var},DNS:${var},"
-done
-SAN=${SAN:0:${#SAN}-1}
 
 # Move to root directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -83,9 +70,7 @@ mkdir -p ${DIR}
 # Create CSR
 openssl req -new -out "${DIR}/$1.csr.pem" \
     -key out/cert.key.pem \
-    -reqexts SAN \
-    -config <(cat ${SSL_CONFIG} \
-        <(printf "[SAN]\nsubjectAltName=${SAN}")) \
+    -config <(cat ${SSL_CONFIG} ) \
     -subj "/C=CN/ST=Guangdong/L=Guangzhou/O=Fishdrowned/OU=$1/CN=*.$1"
 
 # Issue certificate
@@ -107,6 +92,4 @@ ln -snf "../root.crt" "${BASE_DIR}/root.crt"
 echo
 echo "Certificates are located in:"
 
-LS=$([[ `ls --help | grep '\-\-color'` ]] && echo "ls --color" || echo "ls -G")
-
-${LS} -la `pwd`/${BASE_DIR}/*.*
+ls -la `pwd`/${BASE_DIR}/*.*
